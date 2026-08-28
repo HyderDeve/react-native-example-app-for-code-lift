fdfc368bc0f567ccca2ec3536df35e5d0aace724
Hyder <hyder.hashmi21@gmail.com>
2026-08-28 15:23:46 +0500

    dynamic subscriptions relocated because of tabs and tabs layout updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,10 @@ import React from 'react'
 const TabLayout = () => (
     <Tabs screenOptions={{ headerShown: false }}>
         <Tabs.Screen name = "index" options={{title:'Home'}}/>
+        <Tabs.Screen name = "subscriptions" options={{title:'Subscriptions'}}/>
+        <Tabs.Screen name = "insights" options={{title:'Insights'}}/>
+        <Tabs.Screen name = "settings" options={{title:'Settings'}}/>
+        <Tabs.Screen name = "subscriptions/[id]" options={{href : null}}/>
     </Tabs>
 )
 
